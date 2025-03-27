5623a875b3cbb090c1617f8195a7983009450046
leonardo <leonardo>
2025-03-27 13:51:07 -0300

    shadow

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config}  */
+module.exports = {
+  content:["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        primaria: "#FF00FF"
+      }
+    },
+  },
+  plugins: [],
+}
